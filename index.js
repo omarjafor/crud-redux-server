d7bd9fca32984d65fda510018c46ddb893d12bf2
omarjafor <omarjaforchy@gmail.com>
2024-02-03 15:22:17 +0600

    crud redux server side

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.21hcnfr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run(){
+    try{
+        const userCollection = client.db('assetDB').collection('users');
+
+        app.get('/users', async(req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Redux Server is Running')
+})
+
+app.listen(port, () => {
+    console.log('Redux is Running on port', port);
+})
